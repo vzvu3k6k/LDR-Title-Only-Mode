9fb06095e4e2706fc26798ea9de7ecb36cdd15db
vzvu3k6k <vzvu3k6k@gmail.com>
2013-03-16 09:51:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Livedoor Readerでタイトルだけ流し読みするためのユーザースクリプト。`t`キーを押すとタイトルとボタン以外を全部隠して、タイトルの文字を小さくする。もう一度押すと戻る。
+
+フィードごとに状態が記録されるので、毎回`t`を押さなくていい。
+
+[install](https://github.com/vzvu3k6k/LDR-Title-Only-Mode/raw/master/ldr-title-only-mode.user.js)
+
+<table>
+  <tr>
+    <th>Before</th>
+    <td>![default image](http://vzvu3k6k.tk/memo/img/ldr-title-only-mode-default.png)</td>
+  </tr>
+  <tr>
+    <th>After</th></tr>
+    <td>![demo image](http://vzvu3k6k.tk/memo/img/ldr-title-only-mode-custom.png)</td>
+  </tr>
+</dl>
+
+  * フィードごとの状態はどこに記録される？
+    * `localStorage`の`title_only_subids`に、タイトルだけのモードで表示するフィードが`{"<subscription_id>": true, ...}`という形式で保存されている。
+    * 削除したフィードを記録から消す機能はないので、フィードをタイトルだけのモードにしたまま削除するとゴミデータが溜まっていく。
+  * 状態を機能する機能はいらない
+    * `c`キーの本文非表示モードを上書きするユーザースタイルシート [Livedoor Reader: Title-only compact mode - Themes and Skins for Livedoor - userstyles.org](http://userstyles.org/styles/84533/)を使う。
+  * キーの設定を変えたい
+    * `const TITLE_ONLY_KEY = "t";`の`"t"`の中身を適当に書き換える。
---
+++ ldr-title-only-mode.user.js
@@ -0,0 +1,73 @@
+// ==UserScript==
+// @name        LDR Title Only Mode
+// @description Livedoor reader: press "t" to hide everything but titles
+// @version     1.0
+// @namespace   http://www.vzvu3k6k.tk/
+// @homepage    https://github.com/vzvu3k6k/LDR-Title-Only-Mode
+// @match       http://reader.livedoor.com/reader/*
+// @license     public domain
+// ==/UserScript==
+
+GM_addStyle(".title_only .item .item_info," +
+            ".title_only .item .item_body," +
+            ".title_only .item .item_footer {" +
+            "    display: none;" +
+            "}" +
+
+            ".title_only .item .item_title {" +
+            "    font-size: 100% !important;" +
+            "    font-weight: normal;" +
+            "    padding: 5px !important;" +
+            "}" +
+
+            ".title_only .item {" +
+            "    border-bottom: none !important;" +
+            "}" +
+
+            ".title_only .padding {" +
+            "    border: none !important;" +
+            "}");
+
+var main = function(){
+    /* ショートカットキーの設定 */
+    const TITLE_ONLY_KEY = "t";
+    
+    register_hook("AFTER_INIT", function(){
+
+        /* if(mode === undefined) -> toggle
+           if(mode == true)       -> title_only_mode on
+           if(mode == false)      -> title_only_mode off */
+        Control.title_only = function(mode){
+            var subids = JSON.parse(localStorage.getItem("title_only_subids")) || {};
+	        var o = get_active_feed();
+
+            if(mode !== undefined){
+	            (mode ? addClass : removeClass)("right_body", "title_only");
+            }else{
+                toggleClass("right_body", "title_only");
+            }
+
+	        if(contain($("right_body").className, "title_only")){
+		        message("タイトル以外を非表示にしました。" + TITLE_ONLY_KEY + "で元に戻ります");
+                subids[o.subscribe_id] = true;
+	        }else{
+		        message("タイトル以外の非表示を解除しました。" + TITLE_ONLY_KEY + "で非表示に戻ります");
+                delete subids[o.subscribe_id];
+	        }
+
+	        Control.scroll_to_offset(o);
+            localStorage.setItem("title_only_subids", Object.toJSON(subids));
+        };
+
+        Keybind.add(TITLE_ONLY_KEY, function(){
+            Control.title_only();
+        });
+    });
+
+    register_hook("AFTER_PRINTFEED", function(){
+        var subids = JSON.parse(localStorage.getItem("title_only_subids")) || {};
+        Control.title_only(subids[get_active_feed().subscribe_id]);
+    });
+};
+
+location.href = "javascript:void (" + main + ")()";
